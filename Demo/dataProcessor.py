3a36c7a23d11c6a61c77db3a91d9c0b66c736a64
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2022-08-30 02:13:29 +0600

    unintentional change

--- Demo/dataProcessor.py
+++ Demo/dataProcessor.py
@@ -1,4 +1,4 @@
-##BEAT cutting
+# BEAT cutting
 import numpy as np
 from biosppy import storage
 from biosppy.signals import ecg
@@ -12,14 +12,9 @@ import pandas as pd
 
 
 # import required modules
-# import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-# from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout
-# from tensorflow.keras.layers import GlobalMaxPooling2D, MaxPooling2D
-# from tensorflow.keras.layers import BatchNormalization
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.models import load_model
+
 
 
 def beatcutting(channel):
